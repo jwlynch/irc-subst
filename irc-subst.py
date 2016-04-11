593d865ac4771e0201758ec1817fc2e64511c34b
Jim Lynch <jim@jam.sessionsnet.org>
2016-04-10 20:45:51 -0700

    put it in a func

--- irc-subst.py
+++ irc-subst.py
@@ -9,35 +9,35 @@ lookup["[[bar]]"] = "world"
 
 instring = '[[foo]], the message is "[[foo]] [[bar]]"'
 
-# split string, using [[ and ]] as delims
-linelist = re.split(r'(\[{2}|\]{2})', instring)
+sub outLine(inString):
+    # split string, using [[ and ]] as delims
+    linelist = re.split(r'(\[{2}|\]{2})', instring)
 
-long = len(linelist)
+    long = len(linelist)
 
-in_lookup = 0
-lookupKey = ""
-outStr = ""
+    in_lookup = 0
+    lookupKey = ""
+    outStr = ""
 
-for item in range(long):
-  # not in the middle of forming
-  if in_lookup == 0:
-    
-    # beginning of lookup key?
-    if linelist[item] == "[[":
-      lookupKey = "[["
-      in_lookup = 2
-    else: # in the middle of getting lookup key?
-      print("|" + linelist[item] + "|")
-      outStr += linelist[item]
-  else:
-    lookupKey += linelist[item]
-    in_lookup -= 1
-    
-    # we have the whole lookup key?
-    if in_lookup == 0:
-      print("key: " + lookupKey)
-      # here, we would actually do the lookup and append the result
-      outStr += lookup.get(lookupKey)
-      lookupKey = ""
+    for item in range(long):
+        # not forming the lookup key?
+        if in_lookup == 0:
+        
+            # beginning of lookup key?
+            if linelist[item] == "[[":
+                lookupKey = "[["
+                in_lookup = 2
+            else: # symbol is not part of lookup key?
+                outStr += linelist[item]
 
-print("final out: " + outStr)
+        else: # in the middle of forming lookup key?
+            lookupKey += linelist[item]
+            in_lookup -= 1
+        
+        # we have the whole lookup key?
+        if in_lookup == 0:
+            # here, we would actually do the lookup and append the result
+            outStr += lookup.get(lookupKey)
+            lookupKey = ""
+
+print("final out line: " + outLine(inString))
