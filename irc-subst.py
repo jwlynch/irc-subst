454b5f6caee5a2b6bf2520fc93689b2f05932301
Jim Lynch <jim@jam.sessionsnet.org>
2016-10-28 04:10:37 -0700

    class keywordList, to represent key/value pairs, keyword args to funcs, config file options etc

--- irc-subst.py
+++ irc-subst.py
@@ -49,6 +49,22 @@ from subprocess import PIPE
 
 from utils import commandtarget
 
+class KeywordList(object):
+    def __init__(self, properties):
+        self.string = ""
+        self.properties = properties
+
+    def __repr__(self):
+        reslist = []
+
+        for key in self.properties:
+            reslist.append(key + "=" + self.properties[key])
+
+        return " ".join(reslist)
+
+    def attachProp(self, prop, value):
+        self.properties[prop] = value
+                                                                                   
 class irc_subst(commandtarget.CommandTarget):
     def __init__(self, cmdPre):
         self.sent = False
