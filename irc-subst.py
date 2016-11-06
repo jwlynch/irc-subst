14fd6b3fc6777310ef0e99117f3829a061d097fc
Jim Lynch <jim@jam.sessionsnet.org>
2016-11-06 05:05:59 -0800

    move the EATALL result assignment from the end to the start of the block of the if that checks if it's a command

--- irc-subst.py
+++ irc-subst.py
@@ -287,13 +287,12 @@ class irc_subst(commandtarget.CommandTarget):
 
             if len(word) > 0:
                 if word[0].startswith(self.cmdPrefix):
+                    result = hexchat.EAT_ALL
                     cmd = word[0][1:]
                     cmdResult = self.doCommandStr(cmd, word[1:], None)
 
                     if cmdResult == 1:
                         print("command not found")
-
-                    result = hexchat.EAT_ALL
 
             outLineResult = self.outLine("say " + word_eol[0])
             if outLineResult[0]:
