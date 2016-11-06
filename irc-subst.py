dbfbf4c84e2aaa495b0e9f731ca02b23132742e9
Jim Lynch <jim@jam.sessionsnet.org>
2016-11-05 20:28:05 -0700

    call quotizeWordList() in inputHook()

--- irc-subst.py
+++ irc-subst.py
@@ -264,7 +264,7 @@ class irc_subst(commandtarget.CommandTarget):
             if len(word) > 0:
                 if word[0].startswith(self.cmdPrefix):
                     cmd = word[0][1:]
-                    cmdResult = self.doCommandStr(cmd, word[1:], None)
+                    cmdResult = self.doCommandStr(cmd, quotizeWordList(word[1:]), None)
 
                     if cmdResult == 1:
                         print("command not found")
