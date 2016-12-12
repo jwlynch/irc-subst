c0104a0bf334be0add68045bd5e7fb5cadb958dd
Jim Lynch <jim@jam.sessionsnet.org>
2016-12-11 20:30:17 -0800

    finish implementation of cmdDebugSects [ (add | rm) section ] in doCmdString()

--- irc-subst.py
+++ irc-subst.py
@@ -286,8 +286,23 @@ class irc_subst(commandtarget.CommandTarget):
         elif cmdString == self.cmdDebugSects:
             if len(argList) == 0:
                 self.debugPrint("debug sections: " + repr(self.debugSects))
+            elif len(argList) == 2:
+                if argList[0] == "add":
+                    if dex(argList[1], self.debugSects) == -1:
+                        self.debugSects.append(argList[1])
+                        print("debugsects add: %s" % (argList[1]))
+                    else:
+                        print("debugsects add: %s already present" % (argList[1]))
+                elif argList[0] == "rm":
+                    if dex(argList[1], self.debugSects) != -1:
+                        self.debugSects.remove(argList[1])
+                        print("debugsects rm: %s" % (argList[1]))
+                    else:
+                        print("debugsects rm: %s not present" % (argList[1]))
+                else:
+                    print("debugsects: unrecognized subcommand '%s'" % (argList[0]))
             else:
-                self.debugPrint("debug sections: more than one arg not implemented")
+                print("debug sections: wrong number of args")
 
         else:
             # pass buck to superclass
