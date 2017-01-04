af82674278d53e371cafb82e39f3b68d30c36f45
Jim Lynch <jim@jam.sessionsnet.org>
2017-01-04 05:44:30 -0800

    fill in object table definitions

--- sql/objects-dm-create.sql
+++ sql/objects-dm-create.sql
@@ -3,9 +3,25 @@ create sequence object_id_seq;
 
 create table object
 (
+  object_id bigint
+    constraint objects_pk
+      primary key,
+  object_type varchar(100)
+    constraint object_type_nn
+      not null
+    constraint object_type_fk_ob_type
+      references object_type.object_type,
+  creation_date timestamptz
 );
 
 create table object_type
 (
+  object_type varchar(100)
+    constraint object_type_pk
+      primary key
+    constraint object_type_nn
+      not null,
+  extension_table varchar(100),
+  ext_tbl_id_column varchar(100)
 );
 
