18afa70216f6d4e64477e542b84f5d8914ba67df
Jim Lynch <jim@jam.sessionsnet.org>
2017-01-04 18:06:55 -0800

    reverse order of object and object_type tables, as object needs object_type defined before it can be defined

--- sql/objects-tables-create.sql
+++ sql/objects-tables-create.sql
@@ -1,4 +1,15 @@
 
+create table object_type
+(
+  object_type varchar(100)
+    constraint object_type_pk
+      primary key
+    constraint object_type_nn
+      not null,
+  extension_table varchar(100),
+  ext_tbl_id_column varchar(100)
+);
+
 create table object
 (
   object_id bigint
@@ -10,16 +21,5 @@ create table object
     constraint object_type_fk_ob_type
       references object_type(object_type),
   creation_date timestamptz
-);
-
-create table object_type
-(
-  object_type varchar(100)
-    constraint object_type_pk
-      primary key
-    constraint object_type_nn
-      not null,
-  extension_table varchar(100),
-  ext_tbl_id_column varchar(100)
 );
 
