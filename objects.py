f46c2b748d0548006215ce13786f8edd2c465648
Jim Lynch <jim@jam.sessionsnet.org>
2017-03-09 05:32:26 -0800

    take func nextObjectID and make it a standalone func
    remove that function from class Objects, which means Objects will have nothing
    for now. Not ready to use that class now, and dunno exactly how I would.

--- objects.py
+++ objects.py
@@ -1,12 +1,13 @@
-# class and methods for dealing with objects and object types
+# class, methods and funcs for dealing with objects and object types
 
 from sqlalchemy import select, func
-from utils import commandtarget
 
-class Objects(commandtarget.CommandTarget):
-    def nextObjectID(self, conn):
-        seq_sel = select([func.nextval('object_id_seq')])
-        # scalar executes, and gets first col of first row
-        result = conn.scalar(seq_sel)
+def nextObjectID(self, conn):
+    seq_sel = select([func.nextval('object_id_seq')])
+    # scalar executes, and gets first col of first row
+    result = conn.scalar(seq_sel)
 
-        return result
+    return result
+
+class Objects():
+    pass
