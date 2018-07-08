fb6fbe8b7d7948d049dbd08a570ed6710d4a1cca
Jim Lynch <jim@jam.sessionsnet.org>
2018-07-08 03:40:42 -0700

    add default values for object_types__new()

--- sql/test-objtype-func.sql
+++ sql/test-objtype-func.sql
@@ -2,16 +2,15 @@ create or replace function object_type__new(
    p_object_type varchar,
    p_pretty_name varchar,
    p_pretty_plural varchar,
-   p_supertype varchar,
-   p_table_name varchar,           -- default null
-   p_id_column varchar,            -- default null
-   p_package_name varchar,         -- default null
-   p_abstract_p boolean,           -- default 'f'
-   p_type_extension_table varchar, -- default null
-   p_name_method varchar,          -- default null
-   p_create_table_p boolean,       -- default 'f'
-   p_dynamic_p boolean             -- default 'f'
-
+   p_supertype varchar             default 'object',
+   p_table_name varchar            default null,
+   p_id_column varchar             default null,
+   p_package_name varchar          default null,
+   p_abstract_p boolean            default 'f',
+   p_type_extension_table varchar  default null,
+   p_name_method varchar           default null,
+   p_create_table_p boolean        default 'f',
+   p_dynamic_p boolean             default 'f'
 )
 -- final func should return integer
 returns varchar
