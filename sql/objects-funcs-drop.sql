faa0958242a84dc9ad90f7bbc54f77a2f5def745
Jim Lynch <jim@jam.sessionsnet.org>
2018-07-08 02:06:37 -0700

    moved the drop function to a test file temporarily

--- sql/objects-funcs-drop.sql
+++ sql/objects-funcs-drop.sql
@@ -5,18 +5,4 @@
 drop function object_type__delete(
 );
 
-drop function object_type__new(
-   p_object_type varchar,
-   p_pretty_name varchar,
-   p_pretty_plural varchar,
-   p_supertype varchar,
-   p_table_name varchar,           -- default null
-   p_id_column varchar,            -- default null
-   p_package_name varchar,         -- default null
-   p_abstract_p boolean,           -- default 'f'
-   p_type_extension_table varchar, -- default null
-   p_name_method varchar,          -- default null
-   p_create_table_p boolean,       -- default 'f'
-   p_dynamic_p boolean             -- default 'f'
-);
-
+-- put object_type__new dropper here
