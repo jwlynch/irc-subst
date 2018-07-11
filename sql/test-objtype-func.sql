2b4dbfd53c4bcdf60836568726ee186897857362
Jim Lynch <jim@jam.sessionsnet.org>
2018-07-11 04:04:46 -0700

    drop table if desired

--- sql/test-objtype-func.sql
+++ sql/test-objtype-func.sql
@@ -32,6 +32,27 @@ BEGIN
     --     perform acs_attribute__drop_attribute (p_object_type, row.attribute_name);
     --   end loop;
 
+
+    -- Remove the associated table if it exists and p_drop_table_p is true
+
+    if p_drop_table_p then
+
+      select table_name into v_table_name
+      from acs_object_types
+      where object_type = p_object_type;
+
+      if found then
+        if not exists (select 1
+                       from pg_class
+                       where relname = lower(v_table_name)) then
+          raise exception 'Table "%" does not exist', v_table_name;
+        end if;
+
+        execute 'drop table ' || v_table_name || ' cascade';
+      end if;
+
+    end if;
+
   return v_test_out;
 END;
 $$ LANGUAGE plpgsql;
@@ -41,26 +62,6 @@ $$ LANGUAGE plpgsql;
 -- procedure acs_object_type__drop_type/3
 --
 -- (got stuff from here)
---
---   -- Remove the associated table if it exists and p_drop_table_p is true
---
---   if p_drop_table_p then
---
---     select table_name into v_table_name
---     from acs_object_types
---     where object_type = p_object_type;
---
---     if found then
---       if not exists (select 1
---                      from pg_class
---                      where relname = lower(v_table_name)) then
---         raise exception 'Table "%" does not exist', v_table_name;
---       end if;
---
---       execute 'drop table ' || v_table_name || ' cascade';
---     end if;
---
---   end if;
 --
 --   delete from acs_object_types
 --   where object_type = p_object_type;
