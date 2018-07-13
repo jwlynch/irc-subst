564d816afeea065e29f29e628bc97a8b4f597c02
Jim Lynch <jim@jam.sessionsnet.org>
2018-07-13 01:37:47 -0700

    add check constraint and expression if asked for in params

--- sql/test-objtype-func.sql
+++ sql/test-objtype-func.sql
@@ -138,11 +138,11 @@ BEGIN
 
       v_constraint_stub := ' constraint ' || p_object_type || '_' ||
         p_attribute_name || '_';
---
---       if v_datatype.check_expr is not null then
---         v_column_spec := v_column_spec || v_constraint_stub || 'ck check(' ||
---           p_attribute_name || v_datatype.check_expr || ')';
---       end if;
+
+      if v_datatype.check_expr is not null then
+        v_column_spec := v_column_spec || v_constraint_stub || 'ck check(' ||
+          p_attribute_name || v_datatype.check_expr || ')';
+      end if;
 --
 --       if not p_null_p then
 --         v_column_spec := v_column_spec || v_constraint_stub || 'nn not null';
