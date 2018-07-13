a8258f00f06b6149cdcc3c611361b096cb32c3c1
Jim Lynch <jim@jam.sessionsnet.org>
2018-07-13 09:10:39 -0700

    now we're working on func attribute__delete()

--- sql/test-objtype-drop.sql
+++ sql/test-objtype-drop.sql
@@ -1,23 +1,6 @@
-drop FUNCTION attribute__new(
+drop FUNCTION attribute__delete(
    p_object_type varchar,
    p_attribute_name varchar,
-   p_datatype varchar,
-   p_pretty_name varchar,
-   p_pretty_plural varchar,   -- default null
-   p_table_name varchar,      -- default null
-   p_column_name varchar,     -- default null
-   p_default_value varchar,   -- default null
-   p_min_n_values integer,    -- default 1 -- default '1'
-   p_max_n_values integer,    -- default 1 -- default '1'
-   p_sort_order integer,      -- default null
-   p_storage varchar,         -- default 'type_specific'
-   p_static_p boolean,        -- default 'f'
-   p_create_column_p boolean, -- default 'f'
-   p_database_type varchar,   -- default null
-   p_size varchar,            -- default null
-   p_null_p boolean,          -- default 't'
-   p_references varchar,      -- default null
-   p_check_expr varchar,      -- default null
-   p_column_spec varchar      -- default null
+   p_drop_column_p boolean
 
 );
---
+++ sql/test-objtype-func.sql
@@ -0,0 +1,42 @@
+CREATE OR REPLACE FUNCTION attribute__delete(
+   p_object_type varchar,
+   p_attribute_name varchar,
+   p_drop_column_p boolean default 'f'
+
+) RETURNS integer AS $$
+DECLARE
+--   v_table_name             acs_object_types.table_name%TYPE;
+BEGIN
+--
+--   -- Check that attribute exists and simultaneously grab the type's table name
+--   select t.table_name into v_table_name
+--   from acs_object_types t, acs_attributes a
+--   where a.object_type = p_object_type
+--     and a.attribute_name = p_attribute_name
+--     and t.object_type = p_object_type;
+--
+--   if not found then
+--     raise exception 'Attribute %:% does not exist', p_object_type, p_attribute_name;
+--   end if;
+--
+--   -- first remove possible values for the enumeration
+--   delete from acs_enum_values
+--   where attribute_id in (select a.attribute_id
+--                          from acs_attributes a
+--                          where a.object_type = p_object_type
+--                          and a.attribute_name = p_attribute_name);
+--
+--   -- Drop the table if one were specified for the type and we're asked to
+--   if p_drop_column_p and v_table_name is not null then
+--       execute 'alter table ' || v_table_name || ' drop column ' ||
+--         p_attribute_name || ' cascade';
+--   end if;
+--
+--   -- Finally, get rid of the attribute
+--   delete from acs_attributes
+--   where object_type = p_object_type
+--   and attribute_name = p_attribute_name;
+--
+  return 0;
+END;
+$$ LANGUAGE plpgsql;
