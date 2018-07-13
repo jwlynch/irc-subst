8114b3cc33ed76a0be100f20f3b2ff8f77bb3d3e
Jim Lynch <jim@jam.sessionsnet.org>
2018-07-13 09:12:00 -0700

    try dropping objects table after dropping object_types table

--- sql/objects-tables-drop.sql
+++ sql/objects-tables-drop.sql
@@ -1,9 +1,12 @@
 drop table attributes;
 
+drop view attribute_id_seq;
+drop sequence t_attribute_id_seq;
+
 drop table object_type_tables;
 
 drop table datatypes;
 
-drop table objects;
-
 drop table object_types;
+
+drop table objects;
