01b22537e8c511ad4149f318c3e87d46bfd99f28
Jim Lynch <jim@jam.sessionsnet.org>
2018-08-07 04:33:32 -0700

    delete the attributes for 'object' type

--- sql/objects-initData-drop.sql
+++ sql/objects-initData-drop.sql
@@ -1,3 +1,69 @@
+select attribute__delete(
+    p_object_type => 'object',
+    p_attribute_name => 'security_inherit_p',
+    p_drop_column_p => 't'
+);
+
+select attribute__delete(
+    p_object_type => 'object',
+    p_attribute_name => 'title',
+    p_drop_column_p => 't'
+);
+
+select attribute__delete(
+    p_object_type => 'object',
+    p_attribute_name => 'package_id',
+    p_drop_column_p => 't'
+);
+
+select attribute__delete(
+    p_object_type => 'object',
+    p_attribute_name => 'context_id',
+    p_drop_column_p => 't'
+);
+
+select attribute__delete(
+    p_object_type => 'object',
+    p_attribute_name => 'creation_user',
+    p_drop_column_p => 't'
+);
+
+select attribute__delete(
+    p_object_type => 'object',
+    p_attribute_name => 'modifying_user',
+    p_drop_column_p => 't'
+);
+
+select attribute__delete(
+    p_object_type => 'object',
+    p_attribute_name => 'modifying_ip',
+    p_drop_column_p => 't'
+);
+
+select attribute__delete(
+    p_object_type => 'object',
+    p_attribute_name => 'last_modified',
+    p_drop_column_p => 't'
+);
+
+select attribute__delete(
+    p_object_type => 'object',
+    p_attribute_name => 'creation_ip',
+    p_drop_column_p => 't'
+);
+
+select attribute__delete(
+    p_object_type => 'object',
+    p_attribute_name => 'creation_date',
+    p_drop_column_p => 't'
+);
+
+select attribute__delete(
+    p_object_type => 'object',
+    p_attribute_name => 'object_type',
+    p_drop_column_p => 't'
+);
+
 select object_type__delete
 (
     'object',
