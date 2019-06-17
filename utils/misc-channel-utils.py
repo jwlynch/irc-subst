3bb8a698c6506184f6b98ceecee3f7b2db76c104
Jim <jim@jam.sessionsnet.org>
2019-06-17 08:28:39 -0700

    initial commit of misc-channel-utils.py
    working on a way to collect hostmasks into a list,
    to ban, quiet or do something else

---
+++ utils/misc-channel-utils.py
@@ -0,0 +1,4 @@
+masksToBan = "*!*@1.248.57.188 *!*@121.165.250.205 *!*100.net119083074.t-com.ne.jp *!*@119.192.209.120 *!*@119.192.167.20 *!*@y247171.dynamic.ppp.asahi-net.or.jp *!*@p1080172-ipngn200805fukuokachu.fukuoka.ocn.ne.jp *!*@14.34.175.28 *!*@flets-110-092-039-072.fip.synapse.ne.jp *!*@39.122.244.17 *!*@211.208.194.109"
+
+maskList = masksToBan.split()
+print("%s\n" % repr(maskList))
