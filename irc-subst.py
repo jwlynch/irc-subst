3b9d8eff36b1d042759aece84169cb0a45624f3a
Jim <jim@jam.sessionsnet.org>
2020-01-18 08:51:04 -0800

    add regex in var macroname_key_re

--- irc-subst.py
+++ irc-subst.py
@@ -238,6 +238,7 @@ class irc_subst(commandtarget.CommandTarget):
         self.cmdReload = "reload"
 
         self.factoid_key_re = re.compile("^\[\[[a-zA-Z-_]+\]\]$")
+        self.macroname_key_re = re.compile("^[a-zA-Z0-9-_]+$")
         self.channel_re = re.compile("^[#&~].*$")
 
         # initialize superclass
