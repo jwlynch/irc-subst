9e7a85266cce46aec54fd5d6e1436409f7063501
Jim <jim@jam.sessionsnet.org>
2020-01-21 12:18:21 -0800

    copy class KeywordList from irc-subst.py

--- factoids2macros.py
+++ factoids2macros.py
@@ -9,6 +9,22 @@ from sqlalchemy import select, func
 
 from configparser import ConfigParser
 
+class KeywordList(object):
+    def __init__(self, properties):
+        self.string = ""
+        self.properties = properties
+
+    def __repr__(self):
+        reslist = []
+
+        for key in self.properties:
+            reslist.append(key + "=" + self.properties[key])
+
+        return " ".join(reslist)
+
+    def attachProp(self, prop, value):
+        self.properties[prop] = value
+
 class FactoidConverter(object):
     pass
 
