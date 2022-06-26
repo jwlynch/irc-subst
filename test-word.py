d2bd03aa11016803c4400a816d62de2938761f5b
Jim <jim@jam.sessionsnet.org>
2022-06-26 10:28:48 -0700

    add empty input hook function, hook it to hexchat

--- test-word.py
+++ test-word.py
@@ -4,3 +4,8 @@ __module_name__ = "Jim's word test script"
 __module_version__ = "0.0.1d"
 __module_description__ = "word test script"
 
+def inputHook(word, word_eol, userdata):
+    pass
+
+# establish the hook to the input method, immediately above
+hexchat.hook_command('', inputHook)
