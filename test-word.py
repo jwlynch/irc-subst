e2554b805c73396180d8a2c00a205570cab7f113
Jim <jim@jam.sessionsnet.org>
2022-06-26 10:33:23 -0700

    add detailList func

--- test-word.py
+++ test-word.py
@@ -6,6 +6,28 @@ __module_description__ = "word test script"
 
 import hexchat
 
+# return a string detailing a list (its items togeter with each index)
+def detailList(l):
+    # hexchat.prnt("testing detailList:")
+    # test0 = []
+    # test1 = ["one"]
+    # test2 = ["one", "two"]
+    # hexchat.prnt(f"empty: {test0}")
+    # hexchat.prnt(f"1 item: {test1}")
+    # hexchat.prnt(f"2 items: {test2}")
+
+    reslst = []
+
+    for i, item in enumerate(l):
+        reslst.append(f"[{i}]: {item}")
+
+    if len(reslst) != 0:
+        resStr = " ".join(reslst)
+    else: # reslst is empty
+        resStr = "[]"
+
+    return resStr
+
 def inputHook(word, word_eol, userdata):
     pass
 
