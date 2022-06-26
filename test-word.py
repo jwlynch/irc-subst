4ba75e2a6e53abfbe2a88ed26e47221c09a4d083
Jim <jim@jam.sessionsnet.org>
2022-06-26 10:32:01 -0700

    add import for hexchat module

--- test-word.py
+++ test-word.py
@@ -4,6 +4,8 @@ __module_name__ = "Jim's word test script"
 __module_version__ = "0.0.1d"
 __module_description__ = "word test script"
 
+import hexchat
+
 def inputHook(word, word_eol, userdata):
     pass
 
