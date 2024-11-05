e6f8ed0f00788c0c8e39010684cc26c63c16587f
Jim <jim@jam.sessionsnet.org>
2024-11-05 05:58:48 -0800

    adjusted to reflect name change irc-subst.py to irc_subst.py

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ rm -rf $deploy_dir/utils
 
 cp -a utils $deploy_dir/
 
-cp irc-subst.py $deploy_dir/
+cp irc_subst.py $deploy_dir/
 
 # uncomment the following for a small test script:
 # cp test-word.py $deploy_dir/
