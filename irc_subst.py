9956a343554e68cd0f6200ba2c7202d14c9e51d8
Jim <jim@jam.sessionsnet.org>
2024-11-07 23:33:26 -0800

    move class Debugsects into its own file

--- irc_subst.py
+++ irc_subst.py
@@ -38,29 +38,6 @@ def dex(item, lst):
     finally:
         return result
 
-class DebugSectsObj(object):
-    def __init__(self):
-        self.debugSectsList = []
-
-    def debugSectsContains(self, sectName):
-        result = dex(sectName, self.debugSectsList) != -1
-
-        return result
-
-    def addDebugSect(self, addedSect):
-        if not self.debugSectsContains(addedSect):
-            self.debugSectsList.append(addedSect)
-            self.debugPrint(f"debugsects add: {addedSect}")
-        else:
-            self.debugPrint(f"debugsects add: {addedSect} already present")
-
-    def rmDebugSect(self, removedSect):
-        if self.debugSectsContains(removedSect):
-            self.debugSectsList.remove(removedSect)
-            self.debugPrint(f"debugsects rm: {removedSect}")
-        else:
-            self.debugPrint(f"debugsects rm: {removedSect} not present")
-
 # script and config file dir
 
 # nedbat's hack to replace __file__
