5046825dc6b95cdfeb89f8ff55e51a018361b148
Jim <jim@jam.sessionsnet.org>
2024-11-18 05:30:46 -0800

    get rid of def of & call to, irc_subst::makeDebugTab()

--- irc_subst.py
+++ irc_subst.py
@@ -235,8 +235,6 @@ class irc_subst(CommandTarget):
 
         self.debugPrint("scriptPath")
 
-        # self.makeDebugTab()
-
         self.scriptPath = scriptPath
 
         # sqlalchemy
@@ -279,13 +277,6 @@ class irc_subst(CommandTarget):
 
     def debugSectsContains(self, sectName):
         return self.debugSectsObj.debugSectsContains(sectName)
-
-    def makeDebugTab(self):
-        # add the tab for debugging
-        hexchat.command(f"query {self.debugtab_nick}")
-
-        # put the channel list entry for it in the object so I can get at it
-        self.debug_tab = [c for c in hexchat.get_list('channels') if c.channel == self.debugtab_nick][0]
 
     def debugPrint(self, printThis):
         self.debug_tab.debugPrint(printThis)
